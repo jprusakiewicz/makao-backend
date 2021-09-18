7aa08b39f8cc8f73805f5727b845bf6b5f75a2d3
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2021-09-18 16:35:59 +0200

    deck test adapted to a new deck logic

--- test/deck_test.py
+++ test/deck_test.py
@@ -12,13 +12,13 @@ class TestDeck(unittest.TestCase):
         deck = Game.set_full_deck()
         list_length = len(deck)
         self.assertIsInstance(deck, list)
-        self.assertEqual(list_length, 55)
+        self.assertEqual(list_length, 54)
 
     def test_getting_new_game_cards(self):
         deck = Game(2)
         new_game = deck.get_new_game_cards(2)
         print(new_game)
-        self.assertIsInstance(new_game["players"], dict)
+        self.assertIsInstance(new_game, dict)
 
 
 if __name__ == '__main__':
